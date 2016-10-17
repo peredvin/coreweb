abd42c00aa2dfe8fa6c3e7e92107c02d0aa39b61
per h <per_edvin_h@hotmail.com>
2016-10-17 10:32:59 +0200

    first commit after clone to my work pc with studio git plugin

--- coreweb1/src/coreweb1/Startup.cs
+++ coreweb1/src/coreweb1/Startup.cs
@@ -9,40 +9,19 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace coreweb1
 {
+
+
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
-        {
-            app.Run(async (context) =>
-            {
-                await myprocessing(context);
-            });
-        }
-
-        private Task myprocessing(HttpContext context)
-        {
-            if (context.Request.Query.First().ToString().Contains("hubba"))
-                return context.Response.WriteAsync
-                    (string.Format(webpagefixed, context.Request.Query.First().ToString()));
-            else
-           return  context.Response.WriteAsync(webpage); 
-            
-        }
 
         static string webpage =
-            @"<html>
+                    @"<html>
                <body>
                     ingen behandling sker, sidan returneras som det är skriven i html
                </body>
             </html>"
-;
+                ;
 
         static string webpagefixed =
             @"<html>
@@ -51,8 +30,36 @@ namespace coreweb1
                   <script>alert('sidan har manupilerats av kod {0}' ) </script>
               </body>
            </html>"
-;
+        ;
 
+
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                await myprocessing(context);
+            });
+        }
+
+
+        private Task myprocessing(HttpContext context)
+        {
+            if (context.Request.Query.First().ToString().Contains("hubba"))
+                return context.Response.WriteAsync
+                    (string.Format(webpagefixed, context.Request.Query.First().ToString()));
+            else
+
+                return  context.Response.WriteAsync(webpage); 
+            
+        }
+
+        
 
     }
 }
